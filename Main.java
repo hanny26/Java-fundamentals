07d5fd6025664c1dd367b0ea4a2129fa3a310c24
hanny26 <hannyvyas109@gmail.com>
2024-07-09 22:01:45 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        int a = 38;
+        int b = 50;
+        int percentage = a /b*100;
+        System.out.println("percentage of a and b is: " + percentage);
+    }
+}
